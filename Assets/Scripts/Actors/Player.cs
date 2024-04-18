cd3111b10935d37e6fd61909424cfb2d1b8984f7
WiLLLLL683 <32411970+WiLLLLL683@users.noreply.github.com>
2024-04-19 01:23:10 +0800

    RigidBodyUI component

--- Assets/Scripts/Actors/Player.cs
+++ Assets/Scripts/Actors/Player.cs
@@ -8,79 +8,31 @@ namespace SpaceInvadersMiniGame
     public class Player : MonoBehaviour
     {
         [Header("Components")]
-        [SerializeField] private ColliderUI colliderUI;
+        [SerializeField] private RigidBodyUI rigidBodyUI;
 
         private PlayerInput input;
         private PlayerConfig config;
-        private Vector2 targetPosition;
-        private Vector2 velocity;
-
-        private void Awake()
-        {
-            targetPosition = transform.position;
-        }
 
         public void Init(PlayerInput input, PlayerConfig config)
         {
             this.input = input;
             this.config = config;
 
+            rigidBodyUI.Init(config.CollisionThrowBack, config.SmoothTime, config.MaxSpeed);
+
             input.OnMoveInput += Move;
-            colliderUI.OnCollisionEnter += MoveOutOfCollision;
-            colliderUI.OnCollisionStay += MoveOutOfCollision;
         }
 
         private void OnDestroy()
         {
             input.OnMoveInput -= Move;
-            colliderUI.OnCollisionEnter -= MoveOutOfCollision;
-            colliderUI.OnCollisionStay -= MoveOutOfCollision;
-        }
-
-        private void Update()
-        {
-            transform.position = Vector2.SmoothDamp(transform.position, targetPosition, ref velocity, config.SmoothTime, config.MaxSpeed);
         }
 
         private void Move(float horizontalAxis)
         {
-            if (colliderUI.HaveCollisions)
-                return;
-
             float deltaX = horizontalAxis * config.MaxSpeed * Time.deltaTime;
-            targetPosition = transform.position + new Vector3(deltaX, 0);
-        }
-
-        private void MoveOutOfCollision(ColliderUI collider)
-        {
-            float deltaX;
-            if (colliderUI.Center.x > collider.Center.x)
-            {
-                deltaX = collider.xMax - colliderUI.xMin + config.CollisionThrowBack;
-            }
-            else
-            {
-                deltaX = collider.xMin - colliderUI.xMax - config.CollisionThrowBack;
-            }
-
-            float deltaY;
-            if (colliderUI.Center.y > collider.Center.y)
-            {
-                deltaY = collider.yMax - colliderUI.yMin + config.CollisionThrowBack;
-            }
-            else
-            {
-                deltaY = collider.yMin - colliderUI.yMax - config.CollisionThrowBack;
-            }
-
-            if (deltaX > deltaY)
-            {
-                targetPosition = transform.position + new Vector3(deltaX, 0);
-            }
-            else
-            {
-                targetPosition = transform.position + new Vector3(0, deltaY);
-            }
+            var targetPosition = transform.position + new Vector3(deltaX, 0);
+            rigidBodyUI.Move(targetPosition);
         }
     }
 }
---
+++ Assets/Scripts/Tools/PhysicsUI/RigidBodyUI.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+
+namespace CustomUIPhysics
+{
+    [RequireComponent(typeof(ColliderUI))]
+    public class RigidBodyUI : MonoBehaviour
+    {
+        [SerializeField, Min(0.01f)] private float collisionThrowBack;
+        [SerializeField, Min(0.01f)] private float smoothTime;
+        [SerializeField, Min(0f)] private float maxSpeed;
+
+        private ColliderUI colliderUI;
+        private Vector2 targetPosition;
+        private Vector2 velocity;
+
+        private void Awake()
+        {
+            colliderUI = GetComponent<ColliderUI>();
+            targetPosition = transform.position;
+        }
+
+        private void OnEnable()
+        {
+            colliderUI.OnCollisionEnter += MoveOutOfCollision;
+            colliderUI.OnCollisionStay += MoveOutOfCollision;
+        }
+
+        private void OnDisable()
+        {
+            colliderUI.OnCollisionEnter -= MoveOutOfCollision;
+            colliderUI.OnCollisionStay -= MoveOutOfCollision;
+        }
+
+        private void Update()
+        {
+            transform.position = Vector2.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime, maxSpeed);
+        }
+
+        public void Init(float collisionThrowBack, float smoothTime, float maxSpeed)
+        {
+            this.collisionThrowBack = collisionThrowBack;
+            this.smoothTime = smoothTime;
+            this.maxSpeed = maxSpeed;
+        }
+
+        public void Move(Vector2 targetPosition)
+        {
+            if (colliderUI.HaveCollisions)
+                return;
+
+            this.targetPosition = targetPosition;
+        }
+
+        private void MoveOutOfCollision(ColliderUI collider)
+        {
+            float deltaX;
+            if (colliderUI.Center.x > collider.Center.x)
+            {
+                deltaX = collider.xMax - colliderUI.xMin + collisionThrowBack;
+            }
+            else
+            {
+                deltaX = collider.xMin - colliderUI.xMax - collisionThrowBack;
+            }
+
+            float deltaY;
+            if (colliderUI.Center.y > collider.Center.y)
+            {
+                deltaY = collider.yMax - colliderUI.yMin + collisionThrowBack;
+            }
+            else
+            {
+                deltaY = collider.yMin - colliderUI.yMax - collisionThrowBack;
+            }
+
+            if (deltaX > deltaY)
+            {
+                targetPosition = transform.position + new Vector3(deltaX, 0);
+            }
+            else
+            {
+                targetPosition = transform.position + new Vector3(0, deltaY);
+            }
+        }
+    }
+}
